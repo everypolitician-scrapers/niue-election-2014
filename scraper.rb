84af33c7de3fad8b18a17d87919cc27be10ea4a5
Tony Bowden <tony@tmtm.com>
2017-09-22 08:08:42 +0100

    Add 2017 results

--- scraper.rb
+++ scraper.rb
@@ -1,38 +1,31 @@
 #!/bin/env ruby
 # encoding: utf-8
 
-require 'rest-client'
 require 'scraperwiki'
 require 'wikidata/fetcher'
 require 'nokogiri'
 require 'pry'
 
-require 'open-uri/cached'
-OpenURI::Cache.cache_path = '.cache'
-
 def noko_for(url)
-  Nokogiri::HTML(open(URI.escape(URI.unescape(url))).read) 
+  Nokogiri::HTML(open(URI.escape(URI.unescape(url))).read)
 end
 
 def wikinames_from(url)
   noko = noko_for(url)
-  wikinames = []
-  noko.xpath('.//table[.//th[contains(.,"Candidate")]]').each do |table|
+  noko.xpath('.//table[.//th[contains(.,"Candidate")]]').flat_map do |table|
     cols = table.xpath('.//tr[th]/th').map(&:text)
-    table.xpath('.//tr[td]').each do |tr|
+    table.xpath('.//tr[td]').map do |tr|
       tds = tr.css('td')
-      next unless tds.last.text.include? 'Elected'
-      candidate = tds[cols.find_index('Candidate')]
-      wikiname = candidate.xpath('a[not(@class="new")]/@title').text
-      wikinames << wikiname unless wikiname.to_s.empty?
+      next unless tds.last.text.downcase.include? 'elected'
+      tds[cols.find_index('Candidate')].xpath('a[not(@class="new")]/@title').text
     end
-  end
-  raise "No names found in #{url}" if wikinames.count.zero?
-  return wikinames
+  end.compact.reject(&:empty?)
 end
 
-names = wikinames_from('https://en.wikipedia.org/wiki/Niuean_general_election,_2014')
-EveryPolitician::Wikidata.scrape_wikidata(names: { en: names })
+names_2014 = wikinames_from('https://en.wikipedia.org/wiki/Niuean_general_election,_2014')
+names_2017 = wikinames_from('https://en.wikipedia.org/wiki/Niuean_general_election,_2017')
+
+EveryPolitician::Wikidata.scrape_wikidata(names: { en: names_2014 | names_2017 })
 
 
 
