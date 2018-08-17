1b0e7cf73e98bdca15fcbae29a7eccef0843d7ce
Tony Bowden <tony@tmtm.com>
2018-08-17 08:09:40 +0100

    Tidy column headers before comparison
    We need these to include "Candidate", without any leading or trailing
    space.

--- scraper.rb
+++ scraper.rb
@@ -1,10 +1,11 @@
 #!/bin/env ruby
 # encoding: utf-8
 
-require 'scraperwiki'
-require 'wikidata/fetcher'
 require 'nokogiri'
 require 'pry'
+require 'scraped'
+require 'scraperwiki'
+require 'wikidata/fetcher'
 
 def noko_for(url)
   Nokogiri::HTML(open(URI.escape(URI.unescape(url))).read)
@@ -13,7 +14,7 @@ end
 def wikinames_from(url)
   noko = noko_for(url)
   noko.xpath('.//table[.//th[contains(.,"Candidate")]]').flat_map do |table|
-    cols = table.xpath('.//tr[th]/th').map(&:text)
+    cols = table.xpath('.//tr[th]/th').map(&:text).map(&:tidy)
     table.xpath('.//tr[td]').map do |tr|
       tds = tr.css('td')
       next unless tds.last.text.downcase.include? 'elected'
@@ -26,7 +27,3 @@ names_2014 = wikinames_from('https://en.wikipedia.org/wiki/Niuean_general_electi
 names_2017 = wikinames_from('https://en.wikipedia.org/wiki/Niuean_general_election,_2017')
 
 EveryPolitician::Wikidata.scrape_wikidata(names: { en: names_2014 | names_2017 })
-
-
-
-
